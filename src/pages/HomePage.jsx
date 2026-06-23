bb7d1ab72ba5453b753e34079eaebac51716f6d8
manickmanick <manickaperumal22@gmail.com>
2026-06-23 22:30:34 +0530

    basic code done

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+export default function HomePage() {
+  return (
+    <div className="p-10 text-center">
+      <h1 className="text-5xl font-bold text-orange-500">Food Delivery App</h1>
+
+      <p className="mt-4 text-lg text-gray-600">React + Tailwind CSS Project</p>
+    </div>
+  );
+}
